2233b8ebe1a53ed525bafb8d46db31a8076e811d
ecahyo <ecahyono507@gmail.com>
2022-09-20 15:13:56 +0700

    update

---
+++ Registrasi/Identitas.py
@@ -0,0 +1,43 @@
+# import imp
+from json import load
+from lib2to3.pgen2 import driver
+import string
+from turtle import rt
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver import ActionChains
+
+
+from openpyxl import load_workbook
+import time
+
+#target halaman excel ada dimana , wb = variablenya
+# wb = load_workbook(filename="C:\chromedriver\Data.xlsx")
+wb = load_workbook(filename=r"C:\Users\user\Documents\TRCH\Automationpython\Filexel\lainlain.xlsx")
+
+# jadi ini bisa read sheet yang dibawah itu yang di excel
+sheetrange = wb['Harilibur']
+
+# ini web driver disimpen dimana, kalo disimpen di path kosongin aja
+driver = webdriver.Chrome(r'C:\Users\user\Documents\TRCH\chromedriver.exe')
+
+# link nya ini dimana
+driver.get("http://kumbang.torche.id:32400/")
+# seting windows nya jadi max   
+driver.maximize_window()
+# script gakan di eksekusi kalo web ga muncul. kalo lebih dari 10 detik ga muncul error
+driver.implicitly_wait(10)
+# ini letak xpath icon login
+driver.find_element(By.XPATH, "//div/span").click()
+# ini masuk ke form input username
+driver.find_element(By.ID, "username").click()
+# masukin input username
+driver.find_element(By.ID, "username").send_keys("rono")
+# masukin input password
+driver.find_element(By.ID, "password").send_keys("rene")
+# click button login
+driver.find_element(By.ID, "kc-login").click()
+time.sleep(5)
