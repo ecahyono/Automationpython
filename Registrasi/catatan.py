69950071c9e656314d6c32600120a119a34d3da7
Wildan Khaustara <67579895+wildankw@users.noreply.github.com>
2022-10-11 13:52:56 +0700

    Update catatan.py

--- Registrasi/catatan.py
+++ Registrasi/catatan.py
@@ -74,6 +74,8 @@ def test_BarangBawaan(test_setup):
             time.sleep(1)
             driver.find_element(By.CSS_SELECTOR, ".el-input__inner").send_keys(NoInduk)
             time.sleep(3)
+            #WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="el-popper-container-2384"]/div[3]/div/div')))
+            
             driver.find_element(By.CSS_SELECTOR, ".el-select-dropdown__item:nth-child(1) tr:nth-child(2) > .el-descriptions__cell:nth-child(1)").click()
             time.sleep(1)
             #======================== Button Cari ============================
@@ -103,7 +105,8 @@ def test_BarangBawaan(test_setup):
             driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[1]/div[2]/div[2]/div/form/div[2]/div/div[1]/input').send_keys(Keys.ENTER)
 
             driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[1]/div[2]/div[2]/div/form/div[3]/div/div/textarea').send_keys(Deskripsi)
-                
+            
+            driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[1]/div[2]/div[2]/div/form/div[4]/button[2]').click()
            
         except TimeoutException:
             print("MASIH ADA ERROR, CEK LAGI PAK WIL")
