b5b9a63f9de5bae6225810c6823f1ebf8fd538bb
Wildan Khaustara <67579895+wildankw@users.noreply.github.com>
2022-10-12 15:21:38 +0700

    Update PenerimaanTahanan.py

--- Registrasi/PenerimaanTahanan.py
+++ Registrasi/PenerimaanTahanan.py
@@ -1,31 +1,58 @@
-
+from turtle import rt
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver import ActionChains
-from selenium.webdriver.chrome.service import Service
-import pytest
+from selenium.webdriver.common.keys import Keys
 from openpyxl import load_workbook
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+import os
+import pyautogui
+import pytest
 import time
+import platform
+import subprocess
 
-from selenium.webdriver.common.keys import Keys 
+from pathlib import Path
+
 @pytest.fixture()
 def test_setup():
     global driver
-    LokasiChromeDriver = Service('/Users/will/Downloads/chromedriver')
-    #LokasiChromeDriver = Service()
-    driver = webdriver.Chrome(service=LokasiChromeDriver)
-    #url = "http://kumbang.torche.id:32400/"
-    url = "http://192.168.2.11:32400/"
-    driver.get(url)
-    driver.maximize_window()
-    driver.implicitly_wait(10)
-    yield
-    #driver.close()
-    #driver.quit()
-def test_negara(test_setup):
+    global wb
+    swin = Service(r'C:/Users/user/Documents/TRCH/chromedriver.exe')
+    smac = Service('/Users/will/Downloads/chromedriver')
+
+    if platform.system() == 'Darwin':
+        driver = webdriver.Chrome(service=smac)
+        # url = "http://kumbang.torche.id:32400/"
+        url = "http://192.168.2.11:32400/"
+        
+        driver.get(url)
+        # seting windows nya jadi max
+        wb = load_workbook(filename=r"/Users/will/Documents/work/Automationpython/Filexel/Registrasi.xlsx")   
+        driver.maximize_window()
+        driver.implicitly_wait(5)
+        yield
+        driver.close()
+        driver.quit()
+    elif platform.system() == 'Windows':
+        driver = webdriver.Chrome(service=swin)
+        # url = "http://kumbang.torche.id:32400/"
+        url = "http://192.168.2.11:32400/"
+        
+        driver.get(url)
+        # seting windows nya jadi max   
+        wb = load_workbook(filename=r"C:\Users\user\Documents\TRCH\Automationpython\Filexel\Registrasi.xlsx")
+        driver.maximize_window()
+        driver.implicitly_wait(5)
+        yield
+        driver.close()
+        driver.quit()
+        
+def test_PenerimaanTahanan(test_setup):
     
     wb = load_workbook(filename=r"/Users/will/Documents/Automationpython/Filexel/Registrasi.xlsx")
     #wb = load_workbook(filename=r"C:\Users\wilda\Documents\Automationpython\Filexel/Registrasi.xlsx")
