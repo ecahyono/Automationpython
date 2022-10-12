752714ee65bc008a2babf74f96168c99d5539c48
Wildan Khaustara <67579895+wildankw@users.noreply.github.com>
2022-10-12 10:10:30 +0700

    done

--- Registrasi/MeninggalDunia.py
+++ Registrasi/MeninggalDunia.py
@@ -48,10 +48,10 @@ def test_Meningal_Dunia(test_setup):
     actions.move_to_element(element).perform()
     time.sleep(1)
     element2 = driver.find_element(By.XPATH, "//div/ul/li[2]/div")
-    time.sleep(2)
+    time.sleep(1)
     actions2 = ActionChains(driver)
     actions2.move_to_element(element2).perform()
-    time.sleep(2)
+    time.sleep(1)
     driver.find_element(By.LINK_TEXT, "Meninggal Dunia").click()
     WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id=\"app\"]/div/div[2]/div/div[2]/div/div/div[1]/div[1]/h1")))
     #WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id=\"app\"]/div/div[2]/div/div[2]/div/div/div[1]/div[1]/h1")))
@@ -68,49 +68,67 @@ def test_Meningal_Dunia(test_setup):
         TanggalSuratMeninggal                       = sheetrange['E'+str(i)].value
         TanggalMeninggal                            = sheetrange['F'+str(i)].value
         Keterangan                                  = sheetrange['G'+str(i)].value
+        NamaDokumen                                 = sheetrange['H'+str(i)].value
+        DirFile                                     = sheetrange['I'+str(i)].value
+        KeteranganF                                 = sheetrange['J'+str(i)].value
         
         #======================== Halaman Index ============================
-        time.sleep(2)
+    
+        WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id=\"app\"]/div/div[2]/div/div[2]/div/div/div[1]/button')))
+            
         driver.find_element(By.XPATH, "//*[@id=\"app\"]/div/div[2]/div/div[2]/div/div/div[1]/button").click()
-        #WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id=\"app\"]/div/div[2]/div/div[2]/div[1]/div/div/div/h1")))
-        time.sleep(2)
         
         try:      
             #======================== Halaman Cari ===========================
+            WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div/div[2]/div/div[2]/div/div/form/button')))
             driver.find_element(By.CSS_SELECTOR, ".el-input__inner").click()
             time.sleep(1)
             driver.find_element(By.CSS_SELECTOR, ".el-input__inner").send_keys(NoInduk)
             time.sleep(3)
-            #WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="el-popper-container-2384"]/div[3]/div/div')))
             
             driver.find_element(By.CSS_SELECTOR, ".el-select-dropdown__item:nth-child(1) tr:nth-child(2) > .el-descriptions__cell:nth-child(1)").click()
             time.sleep(1)
             #======================== Button Cari ============================
             driver.find_element(By.XPATH, "//*[@id=\"app\"]/div/div[2]/div/div[2]/div[1]/div/form/button[1]").click()
-            #======================== masuk ke halaman input ============================
-            """
-            driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[1]/div[2]/div[2]/div/form/div[1]/div/div/div/div/input').click()
-            if JenisCatatan == 'Catatan Kesehatan' :
-                dropdown = driver.find_element(By.XPATH, "//li[contains(.,'Catatan Kesehatan')]")
-                dropdown.find_element(By.XPATH, "//li[contains(.,'Catatan Kesehatan')]").click()
-            elif JenisCatatan == 'Catatan Perkembangan Pembinaan' :
-                dropdown = driver.find_element(By.XPATH, "//span[contains(.,\'Catatan Perkembangan Pembinaan\')]")
-                dropdown.find_element(By.XPATH, "//span[contains(.,\'Catatan Perkembangan Pembinaan\')]").click()
-            elif JenisCatatan == 'Catatan Prestasi' :
-                dropdown = driver.find_element(By.XPATH, "//span[contains(.,\'Catatan Prestasi\')]")
-                dropdown.find_element(By.XPATH, "//span[contains(.,\'Catatan Prestasi\')]").click()
-            elif JenisCatatan == 'Catatan Penghargaan' :
-                dropdown = driver.find_element(By.XPATH, "//span[contains(.,\'Catatan Penghargaan\')]")
-                dropdown.find_element(By.XPATH, "//span[contains(.,\'Catatan Penghargaan\')]").click()
-            elif JenisCatatan == 'Catatan Barang Titipan' :
-                dropdown = driver.find_element(By.XPATH, "//span[contains(.,\'Catatan Barang Titipan\')]")
-                dropdown.find_element(By.XPATH, "//span[contains(.,\'Catatan Barang Titipan\')]").click()
-            elif JenisCatatan == 'Catatan Lain-Lain' :
-                dropdown = driver.find_element(By.XPATH, "//span[contains(.,\'Catatan Lain-Lain\')]")
-                dropdown.find_element(By.XPATH, "//span[contains(.,\'Catatan Lain-Lain\')]").click()
-            """
+            #======================== Button Cari ============================
             
-           
+
+            driver.find_element(By.CSS_SELECTOR, ".el-col:nth-child(1) .el-input__inner").send_keys(NomorSuratMeninggal)
+            driver.find_element(By.CSS_SELECTOR, ".el-form-item:nth-child(2) .el-textarea__inner").send_keys(TempatMeninggal)
+            driver.find_element(By.CSS_SELECTOR, ".el-col:nth-child(1) > .el-form-item:nth-child(3) .el-textarea__inner").send_keys(SebabMeninggal)
+            driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[1]/div[2]/div[2]/div/form/div[2]/div/div/div[2]/div[1]/div/div/input').send_keys(TanggalSuratMeninggal)
+            driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[1]/div[2]/div[2]/div/form/div[2]/div/div/div[2]/div[1]/div/div/input').send_keys(Keys.ENTER)
+
+            driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[1]/div[2]/div[2]/div/form/div[2]/div/div/div[2]/div[2]/div/div/input').send_keys(TanggalMeninggal)
+            driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[1]/div[2]/div[2]/div/form/div[2]/div/div/div[2]/div[2]/div/div/input').send_keys(Keys.ENTER)
+            driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[1]/div[2]/div[2]/div/form/div[2]/div/div/div[2]/div[3]/div/div/textarea').send_keys(Keterangan)
+
+            driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[1]/div[2]/div[2]/div/div[2]/div[2]/button[2]').click()
+
+            driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[1]/div[2]/div[2]/div/form/div[3]/div/div[2]/div[1]/div[3]/div/div[1]/div/table/tbody/tr/td[2]/div/div/div/div[1]/input').send_keys(NamaDokumen)
+            time.sleep(3)
+            driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[1]/div[2]/div[2]/div/form/div[3]/div/div[2]/div[1]/div[3]/div/div[1]/div/table/tbody/tr/td[3]/div/div/div/div/div/div/button').click()
+            time.sleep(3)
+            pyautogui.write(r"///////users/will/Downloads/pdf/terminologi.pdf")
+            time.sleep(2)
+            pyautogui.press('enter')
+            pyautogui.write(r"///////users/will/Downloads/pdf/terminologi.pdf")
+            time.sleep(3)
+            pyautogui.press('enter')
+            pyautogui.hotkey("backspace")
+            pyautogui.write(r"///////users/will/Downloads/pdf/terminologi.pdf")
+            pyautogui.press('enter')
+            time.sleep(1)
+            pyautogui.press('enter')
+            time.sleep(3)
+            driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[1]/div[2]/div[2]/div/form/div[3]/div/div[2]/div[1]/div[3]/div/div[1]/div/table/tbody/tr/td[4]/div/div/textarea').send_keys(KeteranganF)
+            time.sleep(1)
+            driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[1]/div[2]/div[2]/div/div[2]/div[2]/button[2]').click()
+
+         
+    
+
+
         except TimeoutException:
             print("MASIH ADA ERROR, CEK LAGI PAK WIL")
             pass
