9f57a3666eacdccbf9603a28a58dd70f8e11e005
Wildan Khaustara <67579895+wildankw@users.noreply.github.com>
2022-10-12 15:25:57 +0700

    Update test_negara.py

--- LainLain/Parameter/test_negara.py
+++ LainLain/Parameter/test_negara.py
@@ -5,30 +5,55 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver import ActionChains
-from selenium.webdriver.chrome.service import Service
-
-import pytest
+from selenium.webdriver.common.keys import Keys
 from openpyxl import load_workbook
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+import os
+import pyautogui
+import pytest
 import time
+import platform
+import subprocess
+
+from pathlib import Path
 
 @pytest.fixture()
 def test_setup():
     global driver
-    #s = Service('/Users/will/Downloads/chromedriver')
-    s = Service('location chromedriver')
-    driver = webdriver.Chrome(service=s)
-    url = "http://kumbang.torche.id:32400/"
-    driver.get(url)
-    # seting windows nya jadi max   
-    driver.maximize_window()
-    driver.implicitly_wait(10)
-    yield
-    driver.close()
-    driver.quit()
+    global wb
+    swin = Service(r'C:/Users/user/Documents/TRCH/chromedriver.exe')
+    smac = Service('/Users/will/Downloads/chromedriver')
+
+    if platform.system() == 'Darwin':
+        driver = webdriver.Chrome(service=smac)
+        # url = "http://kumbang.torche.id:32400/"
+        url = "http://192.168.2.11:32400/"
+        
+        driver.get(url)
+        # seting windows nya jadi max
+        wb = load_workbook(filename=r"/Users/will/Documents/work/Automationpython/Filexel/Registrasi.xlsx")   
+        driver.maximize_window()
+        driver.implicitly_wait(5)
+        yield
+        driver.close()
+        driver.quit()
+    elif platform.system() == 'Windows':
+        driver = webdriver.Chrome(service=swin)
+        # url = "http://kumbang.torche.id:32400/"
+        url = "http://192.168.2.11:32400/"
+        
+        driver.get(url)
+        # seting windows nya jadi max   
+        wb = load_workbook(filename=r"C:\Users\user\Documents\TRCH\Automationpython\Filexel\Registrasi.xlsx")
+        driver.maximize_window()
+        driver.implicitly_wait(5)
+        yield
+        driver.close()
+        driver.quit()
+        
 
 def test_negara(test_setup):
-    #wb = load_workbook(filename=r"/Users/will/Documents/Automationpython/Filexel/lainlain.xlsx")
-    wb = load_workbook(filename=r"C:\Users\user\Documents\TRCH\Automationpython\Filexel\lainlain.xlsx")
     sheetrange = wb['NamaNegara']
     driver.find_element(By.XPATH, "//div/span").click()
     # ini masuk ke form input username
