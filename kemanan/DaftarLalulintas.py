33522d6ecc1f21b046602191183dd310da358561
wildankw <wkhaustara@gmail.com>
2022-10-18 10:45:10 +0700

    kemanan

---
+++ kemanan/DaftarLalulintas.py
@@ -0,0 +1,59 @@
+from turtle import rt
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from openpyxl import load_workbook
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+import os
+import pyautogui
+import pytest
+import time
+import platform
+import subprocess
+from pathlib import Path
+
+@pytest.fixture()
+def test_setup():
+    global driver
+    global wb
+    swin = Service(r'C:/Users/user/Documents/TRCH/chromedriver.exe')
+    smac = Service('/Users/will/Downloads/chromedriver')
+
+    if platform.system() == 'Darwin':
+        driver = webdriver.Chrome(service=smac)
+        # url = "http://kumbang.torche.id:32400/"
+        url = "http://192.168.2.11:32400/"
+        
+        driver.get(url)
+        # seting windows nya jadi max
+        wb = load_workbook(filename=r"/Users/will/Documents/work/Automationpython/Filexel/Keamanan.xlsx")   
+        driver.maximize_window()
+        driver.implicitly_wait(5)
+        yield
+        driver.close()
+        driver.quit()
+    elif platform.system() == 'Windows':
+        driver = webdriver.Chrome(service=swin)
+        # url = "http://kumbang.torche.id:32400/"
+        url = "http://192.168.2.11:32400/"
+        
+        driver.get(url)
+        # seting windows nya jadi max   
+        wb = load_workbook(filename=r"C:\Users\user\Documents\TRCH\Automationpython\Filexel\Keamanan.xlsx")
+        driver.maximize_window()
+        driver.implicitly_wait(5)
+        yield
+        driver.close()
+        driver.quit()
+
+    def DaftarLaluLintas():
+        driver.implicitly_wait(10)
+        sheetrange = web [DaftarLaluLintas]
+        
+        
+
