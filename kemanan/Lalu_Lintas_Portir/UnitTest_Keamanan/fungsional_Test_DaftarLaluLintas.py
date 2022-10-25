15178010495326751a49c5a0dd946c19d90f5bec
wildankw <wkhaustara@gmail.com>
2022-10-25 15:02:07 +0700

    asw

--- kemanan/Lalu_Lintas_Portir/UnitTest_Keamanan/fungsional_Test_DaftarLaluLintas.py
+++ kemanan/Lalu_Lintas_Portir/UnitTest_Keamanan/fungsional_Test_DaftarLaluLintas.py
@@ -1,5 +1,3 @@
-
-import os
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -14,7 +12,6 @@ from pytest import mark
 import time
 from pytest_html_reporter import attach
 
-
 @mark.fixture_test()
 def test_setup():
     global driver
@@ -25,16 +22,13 @@ def test_setup():
         driver.get("http://192.168.2.11:32400/")
         driver.maximize_window()
         driver.implicitly_wait(5)
-        print('.')
-        print('==========setup OS Mac berhasil==========')
     elif platform.system() == 'Windows':
         driver = webdriver.Chrome(service=swin)
         driver.get("http://192.168.2.11:32400/")
         driver.maximize_window()
         driver.implicitly_wait(5)
-        print('.')
-        print('==========setup OS Win berhasil==========')
-   
+    print('.')
+    print('==========setup OS berhasil==========')
 
 @mark.fixture_test()
 def test_login():
@@ -42,8 +36,8 @@ def test_login():
     driver.find_element(By.XPATH, "//div/span").click()
     # ini masuk ke form input username
     driver.find_element(By.ID, "username").click()
-    driver.find_element(By.ID, "username").send_keys("wildan")
-    driver.find_element(By.ID, "password").send_keys("wildan")
+    driver.find_element(By.ID, "username").send_keys("test-user")
+    driver.find_element(By.ID, "password").send_keys("password")
     # click button login
     driver.find_element(By.ID, "kc-login").click()
     WebDriverWait(driver, 10)
@@ -54,31 +48,141 @@ def test_login():
 @mark.fixture_test()
 def test_akses_menu():
     driver.implicitly_wait(10)
-    element = driver.find_element(By.XPATH, "//*[@id=\"app\"]/div/nav/ul/li[8]/div")
-    time.sleep(1)
+    nav1 = driver.find_element(By.XPATH, '//*[@id="app"]/div/nav/ul/li[2]/div')
     actions = ActionChains(driver)
-    actions.move_to_element(element).perform()
+    actions.move_to_element(nav1).perform()
+
+    element2 = driver.find_element(By.XPATH, "//div[4]/div/ul/li/div")
     time.sleep(1)
-    element = driver.find_element(By.XPATH, "//*[@id=\"app\"]/div/nav/ul/li[8]/div")
-    actions = ActionChains(driver)
-    actions.move_to_element(element).perform()
-    element2 = driver.find_element(By.XPATH, "//div[10]/div/ul/li/div")
     actions2 = ActionChains(driver)
     actions2.move_to_element(element2).perform()
-    driver.find_element(By.LINK_TEXT, "Daerah Tingkat II").click()
-
+    time.sleep(1)
+    driver.find_element(By.LINK_TEXT, 'Daftar Lalu Lintas').click()
     print('xxx')
-    print('==========akses menu daftar Lain-Lain ==========')
+    print('==========akses menu daftar lalu lintas==========')
+    attach(data=driver.get_screenshot_as_png())
+
+
+@mark.fixture_test()
+def test_search_data_kategori():
+    driver.implicitly_wait(10)
+    WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div/div[2]/div/div[2]/div/div/div[2]/form/div[1]/div/div/button')))
+    WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="filterColumn"]')))
+    driver.find_element(By.XPATH, '//*[@id="filterColumn"]').send_keys('nama')
+    driver.find_element(By.XPATH, '//*[@id="filterColumn"]').click()
+    driver.find_element(By.XPATH, '//*[@id="namaLengkap"]').click()
+    print('.')
+    print('=================================================================================Search Data Form Kategori Berhasil================================================================================= ')
     attach(data=driver.get_screenshot_as_png())
 
 @mark.fixture_test()
-def test_button_tambah():
-    driver.find_element(By.XPATH, "//*[@id=\"app\"]/div/div[2]/div/div[2]/div/div/div[1]/button").click()
-    time.sleep(10)
+def test_clik_clear_value():
+    driver.implicitly_wait(10)
+    driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div/div[2]/div/div/div[2]/form/div[1]/label/div/div/div/span/span/i[2]').click()
+    
+    
+    
+
+    
+    
+    print('.')
+    print('=================================================================================Click Clear Value Button  Berhasil================================================================================= ')
+    attach(data=driver.get_screenshot_as_png())
+
+"""
+@mark.fixture_test()
+def test_search_data_kategori_sesuai():
+    driver.implicitly_wait(10)
+    WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div/div[2]/div/div[2]/div/div/div[2]/form/div[1]/div/div/button')))
+    WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="filterColumn"]')))
+    driver.find_element(By.XPATH, '//*[@id="filterColumn"]').click()
+    driver.find_element(By.XPATH, "//li[contains(.,\'Nama\')]").click()
+    WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="kataKunci"]')))
+    driver.find_element(By.XPATH, '//*[@id="kataKunci"]').send_keys('EYONO BIN CAS')
+    print('.')
+    print('=================================================================================Search Data Sesuai Kategori Berhasil================================================================================= ')
+    attach(data=driver.get_screenshot_as_png())
+
+@mark.fixture_test()
+def test_search_data_kategori_tidak_sesuai():
+    driver.implicitly_wait(10)
+    WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div/div[2]/div/div[2]/div/div/div[2]/form/div[1]/div/div/button')))
+    WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="filterColumn"]')))
+    driver.find_element(By.XPATH, '//*[@id="filterColumn"]').click()
+    driver.find_element(By.XPATH, "//li[contains(.,\'Nama\')]").click()
+    WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="kataKunci"]')))
+    driver.find_element(By.XPATH, '//*[@id="kataKunci"]').send_keys('EYONO BIN CAS')
+    print('.')
+    print('=================================================================================Click Button Tambah Berhasil================================================================================= ')
+    attach(data=driver.get_screenshot_as_png())(10)
+
+@mark.fixture_test()
+def test_search_data_1_kategori_sesuai():
+    driver.implicitly_wait(10)
+
+
+@mark.fixture_test()
+def test_input_varchar():
+    driver.implicitly_wait(10)
+
+@mark.fixture_test()
+def test_sortir_data_table():
+    driver.implicitly_wait(10)
+
+@mark.fixture_test()
+def test_jumlah_perhalaman():
+    driver.implicitly_wait(10)
+
+@mark.fixture_test()
+def test_data_table_sesudah_dan_sebelum():
+    driver.implicitly_wait(10)
+
+@mark.fixture_test()
+def test_input_lebih_dari_jumlah_halaman():
+    driver.implicitly_wait(10)
+
+@mark.fixture_test()
+def test_export_dan_cetak():
+    driver.implicitly_wait(10)
+
+@mark.fixture_test()
+def test_membuka_halaman_tambah():
+    driver.implicitly_wait(10)
+    WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="createButton"]')))
+    driver.find_element(By.XPATH, '//*[@id="createButton"]').click()
+    print('.')
+    print('=================================================================================Click Button Tambah Berhasil=================================================================================')
+    attach(data=driver.get_screenshot_as_png())
+
+@mark.fixture_test()
+def test_membuka_halaman_detail():
+    driver.implicitly_wait(10)
+
+@mark.fixture_test()
+def test_membuka_halaman_ubah():
+    driver.implicitly_wait(10)
+    WebDriverWait(driver,30).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".text-green-500 .h-5")))
+    WebDriverWait(driver,30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".text-green-500 .h-5")))
+    driver.find_element(By.CSS_SELECTOR, ".text-green-500 .h-5").click()
+    print('.')
+    print('=================================================================================Click Button Update Berhasil================================================================================= ')
+    attach(data=driver.get_screenshot_as_png())
+
+
+@mark.fixture_test()
+def test_back_to_top():
+    driver.implicitly_wait(10)
+    WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="createButton"]')))
+    driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[2]').click()
+    print('.')
+    print('=================================================================================Click Button Back To Top Berhasil=================================================================================')
+    attach(data=driver.get_screenshot_as_png())
+"""
 
 
 
 
 def teardown():
+    time.sleep(10)
     driver.close()
     driver.quit()
