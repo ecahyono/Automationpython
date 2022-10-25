9db4c059c05ca3c8b7a4b691edd87c2c045cca42
wildankw <wkhaustara@gmail.com>
2022-10-25 16:03:11 +0700

    bunda corla

--- kemanan/Lalu_Lintas_Portir/UnitTest_Keamanan/fungsional_Test_DaftarLaluLintas.py
+++ kemanan/Lalu_Lintas_Portir/UnitTest_Keamanan/fungsional_Test_DaftarLaluLintas.py
@@ -1,3 +1,4 @@
+from re import T
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -67,29 +68,37 @@ def test_akses_menu():
 def test_search_data_kategori():
     driver.implicitly_wait(10)
     WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div/div[2]/div/div[2]/div/div/div[2]/form/div[1]/div/div/button')))
-    WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="filterColumn"]')))
+    WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="filterColumn"]')))
     driver.find_element(By.XPATH, '//*[@id="filterColumn"]').send_keys('nama')
     driver.find_element(By.XPATH, '//*[@id="filterColumn"]').click()
     driver.find_element(By.XPATH, '//*[@id="namaLengkap"]').click()
+    time.sleep(4)
+    driver.find_element(By.XPATH, '//*[@id="filterColumn"]').click()
+    time.sleep(5)
+    driver.find_element(By.XPATH, '//*[@id="filterColumn"]').send_keys('keluar')
+    driver.find_element(By.XPATH, '//*[@id="keluarKeamanan"]').click()
+    time.sleep(10)
     print('.')
     print('=================================================================================Search Data Form Kategori Berhasil================================================================================= ')
     attach(data=driver.get_screenshot_as_png())
 
+"""
 @mark.fixture_test()
 def test_clik_clear_value():
     driver.implicitly_wait(10)
-    driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div/div[2]/div/div/div[2]/form/div[1]/label/div/div/div/span/span/i[2]').click()
-    
-    
-    
-
-    
-    
+    nav1 = driver.find_element(By.XPATH, '//*[@id="filterColumn"]')
+    actions = ActionChains(driver)
+    actions.move_to_element(nav1).perform()
+    element2 = driver.find_element(By.CSS_SELECTOR, ".el-select__caret:nth-child(2) > svg")
+    time.sleep(1)
+    actions2 = ActionChains(driver)
+    actions2.move_to_element(element2).perform()
+    time.sleep(1)
+    driver.find_element(By.CSS_SELECTOR, ".el-select__caret:nth-child(2) > svg").click()
     print('.')
     print('=================================================================================Click Clear Value Button  Berhasil================================================================================= ')
     attach(data=driver.get_screenshot_as_png())
 
-"""
 @mark.fixture_test()
 def test_search_data_kategori_sesuai():
     driver.implicitly_wait(10)
