42fb9056b61c4607b8534a9b18070c36e7b146c5
Wildan Khaustara <67579895+wildankw@users.noreply.github.com>
2022-11-07 17:54:56 +0700

    Add files via upload

---
+++ Settings/setup.py
@@ -0,0 +1,22 @@
+import platform, sys, json
+from os import environ, path
+from selenium import webdriver
+from pytest import mark
+
+def initDriver():
+    if platform.system() == 'Darwin':
+        driver = webdriver.Chrome(environ.get("CHROMEDRIVERMAC"))
+    elif platform.system() == 'Windows':
+        driver = webdriver.Chrome(environ.get("CHROMEDRIVERWIN"))
+    
+    driver.get(environ.get("HOST"))
+    driver.maximize_window()
+    driver.implicitly_wait(5)
+
+    return driver
+
+def loadDataPath():
+    sys.path.append( '/Users/will/Documents/work/Automationpython' )
+    file = open('data.json', 'r')
+    data = json.load(file)
+    return data
