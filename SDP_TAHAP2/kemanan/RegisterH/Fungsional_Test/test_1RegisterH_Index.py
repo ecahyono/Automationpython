e45a208f54ecd985313d73c4d39abbfe8947774c
wildankw <wkhaustara@gmail.com>
2022-11-14 15:29:14 +0700

    KELUAR MASUK PORTIR

---
+++ SDP_TAHAP2/kemanan/RegisterH/Fungsional_Test/test_1RegisterH_Index.py
@@ -0,0 +1,32 @@
+from distutils.archive_util import make_archive
+from os import PRIO_PGRP, environ
+from re import S, T
+from threading import TIMEOUT_MAX
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.select import Select
+import platform
+from pytest import mark
+import time
+from pytest_html_reporter import attach
+
+import sys
+from pathlib import Path
+
+# file modul
+# from module.setup import initDriver, loadDataPath
+# from module.login import login
+
+sys.path.append("/Users/will/Documents/work/Automationpython")
+from Settings.setup import initDriver, loadDataPath
+from Settings.login import login
+from dotenv import load_dotenv
+
+load_dotenv()
+import json
