efd7061eb4229ac914a75932fe454e326b11d480
wildankw <wkhaustara@gmail.com>
2022-12-09 09:45:39 +0700

    lap

--- SDP_TAHAP2/kemanan/4LaporanBulanan/1.FormulirKamtib6A/test_1Fungsi_Laporan6A_Tambah.py
+++ SDP_TAHAP2/kemanan/4LaporanBulanan/1.FormulirKamtib6A/test_1Fungsi_Laporan6A_Tambah.py
@@ -45,20 +45,30 @@ sheetrange = wb['Laporan6A_Tambah']
 xr = input('')
 i  = xr
 
-filterColumn                          = sheetrange['B'+str(i)].value
-nama                                  = sheetrange['C'+str(i)].value
+filterColumn                            = sheetrange['B'+str(i)].value
+namaNoregis                             = sheetrange['C'+str(i)].value
+inputtahun                              = sheetrange['D'+str(i)].value
+noSkAsimilasi                           = sheetrange['E'+str(i)].value
+tanggalSkAsimilasi                      = sheetrange['F'+str(i)].value
+tanggalAsimilasi                        = sheetrange['G'+str(i)].value
+lokasiAsimilasi                         = sheetrange['H'+str(i)].value
+namaPetugas                             = sheetrange['I'+str(i)].value
+namaPenjamin                            = sheetrange['J'+str(i)].value
+keterlibatanLain                        = sheetrange['K'+str(i)].value
+keterangan                              = sheetrange['L'+str(i)].value
 
 
 @mark.fixture_test()
-def test_1_setupOS():
+def test_1_SetupOS():
     global driver, pathData
     driver = initDriver()
     pathData = loadDataPath()
-    Log.info('Setup Os')
+    Log.info('Login')
+
 
 @mark.fixture_test()
-def test_2_login():
-    loginOperator(driver)
+def test_2_Login():
+    login(driver)
     Log.info('Login')
 
 
@@ -77,27 +87,117 @@ def test_3_akses_menu_index():
 
 @mark.fixture_test()
 def test_4_ClickButtonTambah():
+    #sleep(driver)
     driver.implicitly_wait(60)
     WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="searchButton"]')))
     WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#createButton > span')))
     WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#createButton > span')))
-    sleep(driver)
+    #sleep(driver)
     driver.find_element(By.CSS_SELECTOR, '#createButton > span').click()
     Log.info('Akses Menu Laporan Bulanan')
     attach(data=driver.get_screenshot_as_png())
 
 @mark.fixture_test()
 def test_5_search_Nama():
+    #sleep(driver)
     driver.implicitly_wait(60)
     WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="searchButton"]')))
-    sleep(driver)
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="filterColumn"]')))
+    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="backButton"]')))
+
+    #sleep(driver)
+    driver.find_element(By.ID, 'filterColumn').click()
     if filterColumn == "nama":
         driver.find_element(By.XPATH, '//*[@id="filterColumn"]').send_keys('nama')
         driver.find_element(By.XPATH, "//li[contains(.,\'Nama\')]").click()
     elif filterColumn == "noregis":
         driver.find_element(By.XPATH, '//*[@id="filterColumn"]').send_keys('no')
         driver.find_element(By.XPATH, "//li[contains(.,\'No Registrasi\')]").click()
+
+
+@mark.fixture_test()
+def test_6_input_Nama():
+    #sleep(driver)
+    driver.implicitly_wait(60)
+    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="buttonSearch"]')))
+    driver.find_element(By.XPATH, '//*[@id="kataKunci"]').send_keys(namaNoregis)
+    driver.find_element(By.ID, 'buttonSearch').click()
+
+@mark.fixture_test()
+def test_7_ClickButton_Daftarkan():
+    #sleep(driver)
+    driver.implicitly_wait(60)
+    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="daftarkan0"]')))
+    driver.find_element(By.ID, 'daftarkan0').click()
+
+
+"""
+@mark.fixture_test()
+def test_6_input_TahundanBulanLaporan():
+    #sleep(driver)
+    driver.implicitly_wait(60)
+    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="detailRegis"]')))
+    driver.find_element(By.ID, 'setToday').click()
+    Log.info('menunggu halaman tambah muncul')
+    attach(data=driver.get_screenshot_as_png())
+
+@mark.fixture_test()
+def test_input_NomorSK_Asimilasi():
+    #sleep(driver)
+    driver.implicitly_wait(60)
+    driver.find_element(By.ID, 'noSkAsimilasi').send_keys(noSkAsimilasi)
+    Log.info('input nomor sk asimilasi')
+    attach(data=driver.get_screenshot_as_png())
+
+@mark.fixture_test()
+def test_input_TanggalSk_Asimilasi():
+    #sleep(driver)
+    driver.find_element(By.ID, 'tanggalSkAsimilasi').send_keys(tanggalSkAsimilasi)
+    driver.find_element(By.ID, 'tanggalAsimilasi').send_keys(Keys.ENTER)
+    Log.info('input tanggal sk asimilasi')
+    attach(data=driver.get_screenshot_as_png())
+@mark.fixture_test()
+def test_tanggal_Pelaksanaan_Asimilasi():
+    #sleep(driver)
+    driver.find_element(By.ID, 'tanggalAsimilasi').send_keys(tanggalAsimilasi)
+    driver.find_element(By.ID, 'tanggalAsimilasi').send_keys(Keys.ENTER)
+    Log.info('input tanggal pelaksanaan asimilasi')
+    attach(data=driver.get_screenshot_as_png())
+
+@mark.fixture_test()
+def test_lokasiAsimilasi():
+    #sleep(driver)
+    driver.find_element(By.ID, 'lokasiAsimilasi').send_keys(lokasiAsimilasi)
+    Log.info('input lokasi asimilasi')
+    attach(data=driver.get_screenshot_as_png())
+@mark.fixture_test()
+def test_namaPetugas():
+    #sleep(driver)
+    driver.find_element(By.ID, 'namaPetugas').send_keys(namaPetugas)
+    Log.info('input nama petugas')
+    attach(data=driver.get_screenshot_as_png())
+@mark.fixture_test()
+def test_NamaPenjamin():
+    #sleep(driver)
+    driver.find_element(By.ID, 'namaPenjamin').send_keys(namaPenjamin)
+    Log.info('input Nama Penjamin')
+    attach(data=driver.get_screenshot_as_png())
+
+@mark.fixture_test()
+def test_KeterlibatanPihaklain():
+    #sleep(driver)
+    driver.find_element(By.ID, 'keterlibatanLain').send_keys(keterlibatanLain)
+    Log.info('input Keterlibatan Pihak Lain')
+    attach(data=driver.get_screenshot_as_png())
+@mark.fixture_test()
+def test_Keterangan():
+    #sleep(driver)
+    driver.find_element(By.ID, 'keterangan').send_keys(keterangan)
+    Log.info('input Keterangan')
+    attach(data=driver.get_screenshot_as_png())
+"""
+
+
+
 
 @mark.fixture_test()
 def test_exit():
