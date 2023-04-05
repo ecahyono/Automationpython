ec836ba6801018e5ea176dd050aad8e137be975f
ecahyo <ecahyono507@gmail.com>
2023-04-05 17:01:00 +0700

    rabu05042023

--- SDP/Settings/login.py
+++ SDP/Settings/login.py
@@ -257,3 +257,37 @@ def supcirebon(driver):
     print('========== Login ==========')
     
     attach(data=driver.get_screenshot_as_png())
+
+def lapastesting(driver):
+    WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.ID , 'login')))
+    driver.find_element(By.ID, "login").click()
+    # ini masuk ke form input username
+    driver.find_element(By.ID, "username").click()
+    driver.find_element(By.ID, "username").send_keys("lapastesting")
+    time.sleep(1)
+    driver.find_element(By.ID, "password").send_keys("password")
+    # click button login
+    driver.find_element(By.ID, "kc-login").click()
+    attach(data=driver.get_screenshot_as_png())
+def bpstesting(driver):
+    WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.ID , 'login')))
+    driver.find_element(By.ID, "login").click()
+    # ini masuk ke form input username
+    driver.find_element(By.ID, "username").click()
+    driver.find_element(By.ID, "username").send_keys("bpstesting")
+    time.sleep(1)
+    driver.find_element(By.ID, "password").send_keys("password")
+    # click button login
+    driver.find_element(By.ID, "kc-login").click()
+    attach(data=driver.get_screenshot_as_png())
+def oprupbasaninternal(driver):
+    WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.ID , 'login')))
+    driver.find_element(By.ID, "login").click()
+    # ini masuk ke form input username
+    driver.find_element(By.ID, "username").click()
+    driver.find_element(By.ID, "username").send_keys("oprupbasaninternal")
+    time.sleep(1)
+    driver.find_element(By.ID, "password").send_keys("password")
+    # click button login
+    driver.find_element(By.ID, "kc-login").click()
+    attach(data=driver.get_screenshot_as_png())
