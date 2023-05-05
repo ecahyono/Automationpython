b0669a76ef8da94f50d88dead9e57b360ad83790
ecahyo <ecahyono507@gmail.com>
2023-05-05 16:48:51 +0700

    update git ignore 05052023

---
+++ SDP/SDP_TAHAP2/Bapas/Pendaftaran/src.py
@@ -0,0 +1,54 @@
+from openpyxl import Workbook
+from faker import Faker
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+from pytest_html_reporter import attach
+import os, platform, time, pytest
+from selenium import webdriver
+from os import environ, path
+from pathlib import Path
+from pytest import mark
+import pyautogui
+import platform
+import logging
+import subprocess
+import sys
+from openpyxl import load_workbook
+from dotenv import load_dotenv
+load_dotenv()
+
+if platform.system() == 'Darwin':
+    sys.path.append(environ.get("MACPARENTDIR")) 
+elif platform.system() == 'Windows':
+    sys.path.append(environ.get("WINPARENTDIR"))
+
+from Settings.setupbrowser import initDriver, loadDataPath
+from Settings.login import login, bapasbdg
+from Settings.Page.bapas import daftarklien
+
+Log = logging.getLogger(__name__)
+log_format = '[%(asctime)s %(filename)s->%(funcName)s()]==>%(levelname)s: %(message)s'
+fh = logging.FileHandler('Newrecod.log', mode="w")
+fh.setLevel(logging.INFO)
+formatter = logging.Formatter(log_format)
+fh.setFormatter(formatter)
+Log.addHandler(fh)
+
+#fileexcel
+if platform.system() == 'Darwin':
+    wb = load_workbook(environ.get("excelsatker"))
+elif platform.system() == 'Windows':
+    wb = load_workbook(environ.get("BPSEXEL"))
+
+
+
+
+
+
